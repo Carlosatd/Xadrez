f6830d48f4a37cf1a052a39c366a49b6894faeb6
Carlosatd <carlos_alberto_td@hotmail.com>
2017-05-30 23:57:37 -0300

    Tabuleiro

--- Xadrez/Xadrez.java
+++ Xadrez/Xadrez.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Xadrez;
 
-/**
- *
- * @author antonio
- */
+import javax.swing.JFrame;
+
 public class Xadrez {
-    
+    static String TABULEIRO[][] = {
+        {"r", "n", "b", "q", "k", "b", "n", "r"},
+        {"p", "p", "p", "p", "p", "p", "p", "p"},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " ", " ", " "},
+        {"P", "P", "P", "P", "P", "P", "P", "P"},
+        {"R", "N", "B", "Q", "K", "B", "N", "R"},};
+
+    //Usado para conferir se o rei está em xeque
+    static int posicaoReiMaior, posicaoReiMenor;
+    static int profMax=4;//(prfundidade maxima que vai descer na arvore)
+
+    public static void main(String[] args) {
+        Interface ui = new Interface() {
+        };
+
+        JFrame frame = new JFrame("Xadrez");
+        frame.add(ui);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(512, 534);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 }
